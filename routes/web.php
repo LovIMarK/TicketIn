c223c23d3437d26d4134a372e469df99b737fdb1
LovIMarK <mark@lovink.ch>
2025-06-19 15:19:13 +0200

    correction route

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Aut\RegisterController;
 
 
-Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-
 
 Route::get('/', function () {
     return view('home');
