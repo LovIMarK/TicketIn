c3c6c8cc96b1b4bb33fb11e1c8f30ec61d35b6e7
LovIMarK <mark@lovink.ch>
2025-06-19 14:40:59 +0200

    app config (timezone, app name, language) + projet init (stable version)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Aut\RegisterController;
+
+
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
