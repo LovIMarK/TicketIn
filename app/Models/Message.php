f54b767d88495b4cf3ea43e467e2cbc7dc636ad8
LovIMarK <mark@lovink.ch>
2025-06-29 14:24:01 +0200

    add link between messages and ticket + between messages and attachments

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -7,5 +7,22 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Message extends Model
 {
+
+    /**
+     * Get the ticket that this message belongs to.
+     */
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class);
+    }
+
+    /**
+     * Get the attachments associated with the message.
+     */
+    public function attachments()
+    {
+        return $this->hasMany(Attachment::class);
+    }
+
     use HasFactory;
 }
